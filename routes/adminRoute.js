ba9ff4bbabce18f72c63afd210f22d3ca1f6fed0
tigervanilla <piyushcool102@gmail.com>
2019-01-03 12:49:18 +0530

    admin route update

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,5 +2,11 @@ var router = require('koa-router')();
 var adminCtrl = require('./../controllers/adminCtrl')
 
 router.get('/admin', adminCtrl.showDashboard);
+router.get('/admin-attendance', adminCtrl.attendance);
+router.get('/admin-create-exam', adminCtrl.createNewExam);
+router.get('/admin-fee-status', adminCtrl.showFeeStatus);
+router.get('/admin-result', adminCtrl.showResult);
+// router.get('/admin-update-syllabus', adminCtrl.updateSyllabus);
+router.get('/admin-update-timetable', adminCtrl.updateTimeTable);
 
 module.exports=router;
