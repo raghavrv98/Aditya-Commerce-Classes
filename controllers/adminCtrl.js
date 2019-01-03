9accdebc374040be445b184ccc38f9b2187c4572
tigervanilla <piyushcool102@gmail.com>
2019-01-03 12:48:48 +0530

    all teacher routes set but functionality remains

--- controllers/adminCtrl.js
+++ controllers/adminCtrl.js
@@ -8,5 +8,41 @@ module.exports = {
         else{
             this.redirect('/')
         }
-    }
+    },
+
+    attendance:function*(next){
+        yield this.render('teacher_attendance',{
+            'currentUser':this.currentUser
+        })
+    },
+
+    createNewExam:function*(next){
+        yield this.render('teacher_create_exam',{
+            'currentUser':this.currentUser
+        })
+    },
+
+    showFeeStatus:function*(next){
+        yield this.render('teacher_fee_status',{
+            'currentUser':this.currentUser
+        })
+    },
+
+    showResult:function*(next){
+        yield this.render('teacher_test_result',{
+            'currentUser':this.currentUser
+        })
+    },
+
+    updateTimeTable:function*(next){
+        yield this.render('teacher_update_syllabus',{
+            'currentUser':this.currentUser
+        })
+    },
+
+    // updateSyllabus:function*(next){
+    //     yield this.render('teacher_update_syllabus',{
+    //         'currentUser':this.currentUser
+    //     })
+    // }
 }
