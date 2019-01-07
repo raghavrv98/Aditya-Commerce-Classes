502020be8ac2f1485e6b7de7c30eab79cf6a918a
raghav98 <agrawalraghav669@gmail.com>
2019-01-08 00:32:18 +0530

    change of port no. to 80 and change in config.js that is local host to public ip

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
     redisConfig: {
-        host: '127.0.0.1',
+        host: '13.126.134.2',
         port: 6379
     },
     databaseConnection: {
         connectionLimit: 100,
-        host: '127.0.0.1',
+        host: '13.126.134.2',
         user: 'root',
         password: 'root',
         database: 'aditya2'
