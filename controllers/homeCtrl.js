7c8cdd1edf3ba9272c4497d01304996bdde36354
tigervanilla <piyushcool102@gmail.com>
2019-02-21 23:47:17 +0530

    forgot password crashing when wrong email entered..This bug has now been fixed.

--- controllers/homeCtrl.js
+++ controllers/homeCtrl.js
@@ -1,16 +1,16 @@
 var util = require('util')
 var databaseUtils = require('./../utils/databaseUtils')
 var sessionUtils = require('./../utils/sessionUtils')
-var mailUtils=require('./../utils/mailUtils')
+var mailUtils = require('./../utils/mailUtils')
 module.exports = {
     showHome: function* (next) {
-        if(!this.currentUser){
+        if (!this.currentUser) {
             yield this.render('index', {
-                'currentUser':this.currentUser
+                'currentUser': this.currentUser
             })
-        }else if(this.currentUser.isAdmin){
+        } else if (this.currentUser.isAdmin) {
             this.redirect('/admin')
-        }else{
+        } else {
             this.redirect('/student')
         }
     },
@@ -21,7 +21,7 @@ module.exports = {
         var query = util.format("select * from student where mobile=%s", mobile)
         var result = yield databaseUtils.executeQuery(query)
         var user = result[0]
-        if (user && password === user.password && user.active==1) {
+        if (user && password === user.password && user.active == 1) {
             user["isAdmin"] = false
             sessionUtils.saveUserInSession(user, this.cookies)
             this.redirect('/student')
@@ -44,8 +44,8 @@ module.exports = {
         var newp = this.request.body.newp
         console.log(oldp);
         console.log(newp);
-        
-        var query = util.format('select password from super_user where password="%s"',oldp)
+
+        var query = util.format('select password from super_user where password="%s"', oldp)
         var result = yield databaseUtils.executeQuery(query)
         var user = result[0]
         console.log(user);
@@ -54,26 +54,26 @@ module.exports = {
             console.log(1111111111);
             user["isAdmin"] = true
             console.log("admin");
-            
-            var queryp = util.format('update super_user set password="%s" where password="%s"',newp,oldp)
+
+            var queryp = util.format('update super_user set password="%s" where password="%s"', newp, oldp)
             console.log(queryp);
             var resultp = yield databaseUtils.executeQuery(queryp)
             console.log(resultp);
             sessionUtils.saveUserInSession(user, this.cookies)
             console.log("yahan par aagya hai");
-            
+
             this.redirect('/admin')
         } else {
-            query = util.format('select password from student where password="%s"',oldp)
+            query = util.format('select password from student where password="%s"', oldp)
             result = yield databaseUtils.executeQuery(query)
             user = result[0]
             console.log(user);
-        
+
             if (user && oldp === user.password) {
                 user["isAdmin"] = false
                 console.log("student");
-                
-                var queryp = util.format('update student set password="%s" where password="%s"',newp,oldp)
+
+                var queryp = util.format('update student set password="%s" where password="%s"', newp, oldp)
                 var resultp = yield databaseUtils.executeQuery(queryp)
                 sessionUtils.saveUserInSession(user, this.cookies)
                 this.redirect('/student')
@@ -84,36 +84,45 @@ module.exports = {
 
     },
 
-    logout: function*(next){
+    logout: function* (next) {
         var sessionId = this.cookies.get("SESSION_ID");
-		if (sessionId) {
-			sessionUtils.deleteSession(sessionId);
-		}
-        this.cookies.set("SESSION_ID", '', { expires: new Date(1), path: '/' });
-        this.redirect('/')
-    },
-    forgotPassword:function*(next){
-        var userEmail=this.request.body.email
-        var userMobile=this.request.body.mobile
-        var query=util.format('select email,mobile,password from student where email="%s"',userEmail)
-        var result=yield databaseUtils.executeQuery(query)
-        var isMatch=false;
-        if(result[0].email==userEmail && result[0].mobile==userMobile){
-            mailUtils.sendMail(userEmail,'Password@Aditya Classes',"Your password is: "+result[0].password,'text')
-            isMatch=true
+        if (sessionId) {
+            sessionUtils.deleteSession(sessionId);
         }
-        yield this.render('forgot_password',{
-            'isMatch':isMatch
-        })
-    },
-    
-    enquiryMail:function*(next){
-        var name=this.request.body.name
-        var email=this.request.body.email
-        var mobile=this.request.body.phone
-        var message=this.request.body.message + "\n Email:" + email + "\nMobile:" + mobile + "\nName:" + name
-        mailUtils.sendMail('adityacommerceclasses@gmail.com',"Enquiry Form",message)
+        this.cookies.set("SESSION_ID", '', {
+            expires: new Date(1),
+            path: '/'
+        });
         this.redirect('/')
-    }    
-    
+    },
+    forgotPassword: function* (next) {
+        var userEmail = this.request.body.email
+        var userMobile = this.request.body.mobile
+        var query = util.format('select email,mobile,password from student where mobile="%s"', userMobile)
+        var result = yield databaseUtils.executeQuery(query)
+        var isMatch = false;
+        if (result.length == 1) {
+            if (result[0].email == userEmail && result[0].mobile == userMobile) {
+                mailUtils.sendMail(userEmail, 'Password@Aditya Classes', "Your password is: " + result[0].password, 'text')
+                isMatch = true
+            }
+            yield this.render('forgot_password', {
+                'isMatch': isMatch
+            })
+        } else {
+            yield this.render('forgot_password', {
+                'isMatch': isMatch
+            })
+        }
+    },
+
+    enquiryMail: function* (next) {
+        var name = this.request.body.name
+        var email = this.request.body.email
+        var mobile = this.request.body.phone
+        var message = this.request.body.message + "\n Email:" + email + "\nMobile:" + mobile + "\nName:" + name
+        mailUtils.sendMail('adityacommerceclasses@gmail.com', "Enquiry Form", message)
+        this.redirect('/')
+    }
+
 }
